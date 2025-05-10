0d76b9a98db285e18d91eab1d3c167c8b69c4b06
Abhishek <abhishekpandeybhu@gmail.com>
2025-05-10 23:21:39 +0530

    change the photo of the product

--- src/App.jsx
+++ src/App.jsx
@@ -16,93 +16,93 @@ function App() {
   const Raw = [
     {
       image:
-        "https://rukminim2.flixcart.com/image/612/612/xif0q/hair-oil/n/o/4/250-neelgiri-herbal-hair-oil-hair-growth-hair-oil-adivasi-original-imah3mwjgbqm4kvq.jpeg?q=70",
+        "https://adivasioriginals.com/wp-content/uploads/2024/12/Adivasi-Originals-Organic-Herbal-Hair-Oil-Photo-1-scaled-0x0.webp",
       name: "Adivasi Hair Oil",
-      price: 1000,
+      price: 2000,
       added: false,
       id: 1,
       quantity: 1,
-      amount: 1000
+      amount: 2000,
     },
     {
       image:
-        "https://rukminim2.flixcart.com/image/612/612/xif0q/hair-oil/n/o/4/250-neelgiri-herbal-hair-oil-hair-growth-hair-oil-adivasi-original-imah3mwjgbqm4kvq.jpeg?q=70",
+        "https://adivasioriginals.com/wp-content/uploads/2025/02/Aivasi-Hair-Oil-Photos-0x0.webp",
       name: "Adivasi Hair Oil",
-      price: 1000,
+      price: 1500,
       added: false,
       id: 2,
       quantity: 1,
-      amount: 1000
+      amount: 1500,
     },
     {
       image:
-        "https://rukminim2.flixcart.com/image/612/612/xif0q/hair-oil/n/o/4/250-neelgiri-herbal-hair-oil-hair-growth-hair-oil-adivasi-original-imah3mwjgbqm4kvq.jpeg?q=70",
+        "https://adivasioriginals.com/wp-content/uploads/2024/09/produt-photo-1-0x0.webp",
       name: "Adivasi Hair Oil",
       price: 1000,
       added: false,
       id: 3,
       quantity: 1,
-      amount: 1000
+      amount: 1000,
     },
     {
       image:
-        "https://rukminim2.flixcart.com/image/612/612/xif0q/hair-oil/n/o/4/250-neelgiri-herbal-hair-oil-hair-growth-hair-oil-adivasi-original-imah3mwjgbqm4kvq.jpeg?q=70",
+        "https://adivasioriginals.com/wp-content/uploads/2024/04/Hair-Oil-Product-Slide-3-scaled-0x0.webp",
       name: "Adivasi Hair Oil",
       price: 1000,
       added: false,
       id: 4,
       quantity: 1,
-      amount: 1000
+      amount: 1000,
     },
     {
       image:
-        "https://rukminim2.flixcart.com/image/612/612/xif0q/hair-oil/n/o/4/250-neelgiri-herbal-hair-oil-hair-growth-hair-oil-adivasi-original-imah3mwjgbqm4kvq.jpeg?q=70",
+        "https://adivasioriginals.com/wp-content/uploads/2024/04/Hair-Oil-Product-Slide-2-scaled-0x0.webp",
       name: "Adivasi Hair Oil",
       price: 1000,
       added: false,
       id: 5,
       quantity: 1,
-      amount: 1000
+      amount: 1000,
     },
     {
       image:
-        "https://rukminim2.flixcart.com/image/612/612/xif0q/hair-oil/n/o/4/250-neelgiri-herbal-hair-oil-hair-growth-hair-oil-adivasi-original-imah3mwjgbqm4kvq.jpeg?q=70",
+        "https://adivasioriginals.com/wp-content/uploads/2024/07/Adivasi-Originals-Hair-Oil-Photo-26.jpeg",
       name: "Adivasi Hair Oil",
       price: 2000,
       added: false,
       id: 6,
       quantity: 1,
-      amount: 2000
+      amount: 2000,
     },
     {
       image:
-        "https://rukminim2.flixcart.com/image/612/612/xif0q/hair-oil/n/o/4/250-neelgiri-herbal-hair-oil-hair-growth-hair-oil-adivasi-original-imah3mwjgbqm4kvq.jpeg?q=70",
+        "https://adivasioriginals.com/wp-content/uploads/2024/07/Adivasi-Originals-Hair-Oil-Photo-5.jpeg",
       name: "Adivasi Hair Oil",
-      price: 9000,
+      price: 1500,
       added: false,
       id: 7,
       quantity: 1,
-      amount:9000
+      amount: 1500,
     },
     {
       image:
         "https://rukminim2.flixcart.com/image/612/612/xif0q/hair-oil/n/o/4/250-neelgiri-herbal-hair-oil-hair-growth-hair-oil-adivasi-original-imah3mwjgbqm4kvq.jpeg?q=70",
       name: "Adivasi Hair Oil",
-      price: 7000,
+      price: 1200,
       added: false,
       id: 8,
       quantity: 1,
-      amount: 7000
+      amount: 1200,
     },
     {
       image:
-        "https://rukminim2.flixcart.com/image/612/612/xif0q/hair-oil/n/o/4/250-neelgiri-herbal-hair-oil-hair-growth-hair-oil-adivasi-original-imah3mwjgbqm4kvq.jpeg?q=70",
+        "https://adivasioriginals.com/wp-content/uploads/2024/07/Adivasi-Originals-Hair-Oil-Photo-6.jpeg",
       name: "Adivasi Hair Oil",
       price: 1000,
       added: false,
       id: 9,
       quantity: 1,
-      amount: 1000
+      amount: 1000,
     },
 
     // Add more items as needed
@@ -124,7 +124,7 @@ function App() {
     city: "",
     state: "",
   });
-console.log(address)
+  console.log(address);
   // Sidebar toggle function
   const toggleSidebar = () => {
     setSidebarOpen((prev) => !prev);
@@ -144,13 +144,18 @@ console.log(address)
   return (
     <div className="flex h-screen">
       {/* Sidebar */}
-      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+      <Sidebar
+        isOpen={isSidebarOpen}
+        toggleSidebar={toggleSidebar}
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+      />
 
       {/* Main Content */}
       <div className="flex flex-col w-full">
         {/* Navbar */}
         <Navbar
-        setAddressSaved={setAddressSaved}
+          setAddressSaved={setAddressSaved}
           toggleSidebar={toggleSidebar}
           isLoggedIn={isLoggedIn}
           setIsLoggedIn={setIsLoggedIn}
@@ -180,7 +185,15 @@ console.log(address)
             <Route
               path="/cart"
               element={
-                <Cart   setCartItems={setCartItems} setCount={setCount} address={address}  addressSaved={addressSaved} setAddressSaved={setAddressSaved}cartItems={cartItems} isLoggedIn={isLoggedIn} />
+                <Cart
+                  setCartItems={setCartItems}
+                  setCount={setCount}
+                  address={address}
+                  addressSaved={addressSaved}
+                  setAddressSaved={setAddressSaved}
+                  cartItems={cartItems}
+                  isLoggedIn={isLoggedIn}
+                />
               }
             />
             <Route
@@ -189,7 +202,17 @@ console.log(address)
             />
             <Route path="/signup" element={<Signup />} />
             <Route path="/term" element={<TermsAndConditions />} />
-            <Route path="/address" element={<AddressSaver address={address} setAddress={setAddress} addressSaved={addressSaved} setAddressSaved={setAddressSaved}></AddressSaver>} />
+            <Route
+              path="/address"
+              element={
+                <AddressSaver
+                  address={address}
+                  setAddress={setAddress}
+                  addressSaved={addressSaved}
+                  setAddressSaved={setAddressSaved}
+                ></AddressSaver>
+              }
+            />
           </Routes>
         </div>
 
